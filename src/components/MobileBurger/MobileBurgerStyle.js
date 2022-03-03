26812b0ba6bc563d39b6e8075379ac961e2f91b7
FLorin <florinainol@gmail.com>
2022-03-03 20:37:52 +0200

    Navigation list + effects

--- src/components/MobileBurger/MobileBurgerStyle.js
+++ src/components/MobileBurger/MobileBurgerStyle.js
@@ -4,6 +4,7 @@ export const MobileBurgerContainer = styled.div`
   position: fixed;
   top: 48px;
   right: 20px;
+  z-index:9999;
   @media only screen and (min-width: 950px) {
     display: none;
   }
@@ -14,7 +15,9 @@ export const MobileBurger = styled.div`
   position: relative;
   width: 30px;
   height: 2px;
-  background-color: ${ props=> props.defaultColor};
+  z-index:150;
+  background-color: ${(props) =>
+    props.openedBurger == true ? "transparent" : "white"};
 
   &::after,
   &::before {
@@ -23,13 +26,59 @@ export const MobileBurger = styled.div`
     width: inherit;
     height: inherit;
     background-color: white;
+    transition: 0.2s;
   }
 
   &::after {
-    top: 7px;
+    top: ${(props) => (props.openedBurger == true ? "0px" : "7px")};
+    transform: rotate(
+      ${(props) => (props.openedBurger == true ? "135deg" : "0")}
+    );
   }
 
   &::before {
-    top: -7px;
+    top: ${(props) => (props.openedBurger == true ? "0px" : "-7px")};
+    transform: rotate(
+      ${(props) => (props.openedBurger == true ? "-135deg" : "0")}
+    );
   }
 `;
+
+export const NavBckg = styled.div`
+  position:fixed;
+  width: 1px;
+  height: 1px; ;
+  border-radius: 50%;
+  background-image: radial-gradient(#0f1624, #212D45);
+  transition: transform 0.8s cubic-bezier(0.86, 0, 0.7, 1);
+  top: 48px;
+  right: 20px;
+  transform: scale(${(props) => (props.openedBurger == true ? "10000" : "0")});
+`;
+
+export const NavigationListCnt = styled.div `
+  position:fixed;
+  transition: all 0.8s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  transform:translateX(${(props) => (props.openedBurger == true ? "0" : "-800px")}); 
+`
+
+export const NavigationList = styled.ul `
+top:50vh;
+left:50vw;
+transform:translateX(-70%);
+width:100vw;
+height:100vh;
+
+
+  &>li {
+    color:white;
+    font-size:3.7rem;
+    text-transform:uppercase;
+    line-height:100px;
+
+    &>a {
+      text-decoration:none;
+      color:white;
+    }
+  }
+`
--- src/components/MobileBurger/index.js
+++ src/components/MobileBurger/index.js
@@ -1,12 +1,27 @@
-import { Div1 } from "../Header/HeaderStyles"
+import { useState } from "react";
 
-import { MobileBurgerContainer, MobileBurger } from "./MobileBurgerStyle";
+import { MobileBurgerContainer, MobileBurger, NavBckg , NavigationList, NavigationListCnt} from "./MobileBurgerStyle";
 
 const MobileNavigation = ()=> {
+    const [ opened , setOpened] = useState(false);  
+
+    function navigationAction() {
+        setOpened(!opened);
+    } 
 
     return (
         <MobileBurgerContainer>
-            <MobileBurger defaultColor="red"></MobileBurger>
+            <NavBckg openedBurger={opened} />
+            {/* <div style={{"width":'200px', position:"absolute", height:"200px", backgroundColor:"red"}}></div> */}
+            <MobileBurger openedBurger={opened} onClick={navigationAction}></MobileBurger>
+           <NavigationListCnt openedBurger={opened}>
+                <NavigationList>
+                    <li><a href="#projects" onClick={navigationAction}>Projects</a></li>
+                    <li><a href="#tech" onClick={navigationAction}>Technologies</a></li>
+                    <li><a href="#resources" onClick={navigationAction}>Resources</a></li>
+                    <li><a href="#about" onClick={navigationAction}>About</a></li>
+                </NavigationList>
+            </NavigationListCnt>
         </MobileBurgerContainer>
     )
 }
